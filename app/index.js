dd966855044d862dc0c1368667b0b2c7ad2b4543
vkuldeep1 <kuldeep.signaturetech@gmail.com>
2026-03-31 09:03:50 +0000

    First commit of this lab

---
+++ app/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello DevSecOps");
+});
+
+// Vulnerability: command injection risk
+app.get('/ping', (req, res) => {
+  const { exec } = require('child_process);
+  const host = req.query.host;
+
+  exec(`ping -c 1 ${host}`, (err, stdout) => {
+    res.send(stdout);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
+
