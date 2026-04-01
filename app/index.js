6a5154de28191cc0ab905e8d044a9791878c26bc
vkuldeep1 <kuldeep.signaturetech@gmail.com>
2026-04-01 07:21:03 +0000

    Application updation

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { spawn } = require('child_process');
 
 app.get('/', (req, res) => {
   res.send("Hello DevSecOps");
@@ -7,13 +8,21 @@ app.get('/', (req, res) => {
 
 // Vulnerability: command injection risk
 app.get('/ping', (req, res) => {
-  const { exec } = require('child_process');
-  const host = req.query.host;
+    const host = req.query.host;
 
-  exec(`ping -c 1 ${host}`, (err, stdout) => {
-    res.send(stdout);
-  });
-});
+    // Basic validation (strict)
+    if (!/^[a-zA-Z0-9.-]+$/.test(host)) {
+        return res.status(400).send("Invalid host");
+    }
+
+    const ping = spawn('ping', ['-c', '1', host]);
+
+    let output = '';
+
+    ping.stdout.on('data', (data) => {
+        output += data.toString();
+    });
+
 
 app.listen(3000, () => {
   console.log("App running on port 3000");
